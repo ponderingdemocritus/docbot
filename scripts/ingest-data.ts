6da412b05205a2ca0eb5ab073b1ef68c5ca0ef3a
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-03-21 14:38:01 +1100

    init

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -2,16 +2,43 @@ import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { OpenAIEmbeddings } from 'langchain/embeddings';
 import { PineconeStore } from 'langchain/vectorstores';
 import { pinecone } from '@/utils/pinecone-client';
-import { PDFLoader } from 'langchain/document_loaders';
+// import { PDFLoader } from 'langchain/document_loaders';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
+import { TextLoader } from "langchain/document_loaders";
+
+import fs from 'fs';
+import path from 'path';
 
 /* Name of directory to retrieve files from. You can change this as required */
-const filePath = 'docs/MorseVsFrederick.pdf';
+// const filePath = 'docs/230310130.pdf';
 
-export const run = async () => {
+const processDirectory = async (directoryPath: any) => {
+  try {
+    const items = await fs.promises.readdir(directoryPath);
+
+    for (const item of items) {
+      const itemPath = path.join(directoryPath, item);
+      const stats = await fs.promises.stat(itemPath);
+
+      if (stats.isDirectory()) {
+        await processDirectory(itemPath);
+      } else if (stats.isFile() && path.extname(itemPath) === '.adoc') {
+        console.log(`Processing file: ${itemPath}`);
+        await run(itemPath);
+        console.log('Ingestion complete for', itemPath);
+      }
+    }
+  } catch (error) {
+    console.log('Error processing directory:', error);
+    throw new Error('Failed to process the directory');
+  }
+};
+
+export const run = async (filePath: string) => {
   try {
     /*load raw docs from the pdf file in the directory */
-    const loader = new PDFLoader(filePath);
+    // const loader = new PDFLoader(filePath);
+    const loader = new TextLoader(filePath);
     // const loader = new PDFLoader(filePath);
     const rawDocs = await loader.load();
 
@@ -45,6 +72,7 @@ export const run = async () => {
 };
 
 (async () => {
-  await run();
+  const rootDirectoryPath = '/home/os/Documents/GPT/starknet-docs'
+  await processDirectory(rootDirectoryPath);
   console.log('ingestion complete');
 })();
